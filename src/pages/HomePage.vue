bd37fbbc29b0fd588771e20e11403ae675bfb3e2
Rhys Vickers <rs.vickers5@gmail.com>
2023-12-20 14:56:18 -0700

    Started a landing page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Yoooo its ya boi Rhys. I have a landing page!</h1>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: clamp(500px, 50vw, 100%);
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
