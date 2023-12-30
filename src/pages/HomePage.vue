dab84f957162eb12c539508d4a199edf6627b847
Rhys Vickers <rs.vickers5@gmail.com>
2023-12-29 17:31:12 -0700

    working on my landing page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container-fluid">
     <section class="row">
+      <div class="col-md-9 col-12">
+        Hi
+      </div>
+      <div class="col-md-3 col-12 d-flex justify-content-center maroonBg">
+        <div class="text-light">
+          <img class="rounded-pill avatar p-4" src="../assets/img/Rhys-Vickers-4.jpg" alt=""
+            title="Click for a fun fact about Rhys!" role="button">
+          <h3>Rhys Vickers</h3>
+          <h5>Software Developer</h5>
+
+        </div>
+      </div>
 
     </section>
   </div>
 </template>
-
-<!-- <h1>Yoooo its ya boi Rhys. I have a landing page!</h1>
-<img class="rounded-circle" src="../assets/img/Rhys-Vickers-4.jpg" alt="" title="Rhys Vickers"> -->
 <script>
 export default {
   setup() {
@@ -19,8 +28,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
-img {
-  height: 15dvh;
-  width: 15dvh;
+.avatar {
+  height: 24dvh;
+  width: 24dvh;
+}
+
+.maroonBg {
+  background-color: rgba(93, 0, 0, 0.923);
+  height: 400dvh;
+
 }
 </style>
