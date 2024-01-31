bd64945964a16dd5e4d21b1511bf103eb9d6d994
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-01-31 08:58:23 -0700

    push

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <section class="row">
       <div class="col-12">
         I may take a small break. I kind of want to continue to write my novel. But I also need a job really bad. I think
-        I may try to do an apex themed project.
+        I may try to do an apex themed project. I may have a small job!
       </div>
     </section>
   </div>
